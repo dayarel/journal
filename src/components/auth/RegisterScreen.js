98c7f14fdc586aca6e72235aa04e3a5ef02111ee
David Cortes <caravanasar@gmail.com>
2021-01-20 18:26:01 +0100

    Register Screen stylings

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,10 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const RegisterScreen = () => {
   return (
-    <div>
-      <h1>Register Scren</h1>
-    </div>
+    <>
+      <h3 className="auth__title">Register</h3>
+      <form>
+        <input
+          type="text"
+          className="auth__input skew-dark"
+          placeholder="Name"
+          name="name"
+          autoComplete="off"
+        />
+        <input
+          type="text"
+          className="auth__input skew-dark"
+          placeholder="Your email"
+          name="email"
+          autoComplete="off"
+        />
+        <input
+          type="password"
+          className="auth__input skew-dark"
+          placeholder="Password"
+          name="password"
+        />
+        <input
+          type="password"
+          className="auth__input skew-dark"
+          placeholder="Confirm Password"
+          name="confirm"
+        />
+        <button type="submit" className="btn btn-primary btn-block">
+          Register
+        </button>
+        <Link to="/auth/login" className="link animated__underline">
+          Already registered?
+        </Link>
+      </form>
+    </>
   );
 };
 
