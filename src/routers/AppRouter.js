48bf38dee1a4302058208c7f3250de953d70996e
David Cortes <caravanasar@gmail.com>
2021-01-30 23:54:09 +0100

    Routing validation

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,34 +1,49 @@
-import React, { useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { firebase } from "../firebase/firebase-config";
+import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
 import AuthRouter from "./AuthRouter";
+import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
+import { firebase } from "../firebase/firebase-config";
 import JournalScreen from "../components/journal/JournalScreen";
 import { login } from "../actions/auth";
 
 const AppRouter = () => {
   const dispatch = useDispatch();
 
+  const [checking, setChecking] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user?.uid) {
         dispatch(login(user.uid, user.displayName, user.photoURL));
+        setIsLoggedIn(true);
       } else {
+        setIsLoggedIn(false);
       }
+      setChecking(false);
     });
-  }, [dispatch]);
+  }, [dispatch, setChecking, setIsLoggedIn]);
+
+  if (checking) {
+    return <h1>Wait...</h1>;
+  }
 
   return (
     <Router>
       <div>
         <Switch>
-          <Route path="/auth" component={AuthRouter} />
-          <Route exact path="/" component={JournalScreen} />
+          <PublicRoute
+            path="/auth"
+            component={AuthRouter}
+            isAuthenticated={isLoggedIn}
+          />
+          <PrivateRoute
+            exact
+            path="/"
+            component={JournalScreen}
+            isAuthenticated={isLoggedIn}
+          />
           <Redirect to="/auth/login" />
         </Switch>
       </div>
