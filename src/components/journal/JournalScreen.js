61e735c594e71e97db671d3f034fde27f5201cb5
David Cortes <caravanasar@gmail.com>
2021-02-01 21:57:32 +0100

    Created NotesReducer

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
-// import NothingSelected from "./NothingSelected";
+import { useSelector } from "react-redux";
+import NothingSelected from "./NothingSelected";
 import NoteScreen from "../notes/NoteScreen";
 const JournalScreen = () => {
+  const { active } = useSelector((state) => state.notes);
+
   return (
     <>
       <Navbar />
       <div className="journal__main-content">
         <Sidebar />
-        <main>
-          <NoteScreen />
-        </main>
+        <main>{active ? <NoteScreen /> : <NothingSelected />}</main>
       </div>
     </>
   );
